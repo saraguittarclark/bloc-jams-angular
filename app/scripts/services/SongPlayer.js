d1a98cf690b76250c6b48b2001d1290ada9a273b
Sara Clark <sara.guittar@gmail.com>
2016-03-13 22:48:19 -0400

    clicks not working

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,14 +2,74 @@
 	function SongPlayer() {
 		var SongPlayer = {};
 	
+	var currentSong = null;
 
-	SongPlayer.play = function(song) {
-		var currentBuzzObject = new buzz.sound(song.audioUrl, {
+	/**
+	@desc Buzz Object audio file
+	@type {Object}
+	**/
+
+	var currentBuzzObject = null;
+
+	/**
+	@function setSong
+	@desc Stops currently playing song and loads new audio file as currentBuzzObject
+	@param {Object} song
+	**/
+
+	var setSong = function(song) {
+		if (currentBuzzObject) {
+			currentBuzzObject.stop();
+			currentSong.playing = null;
+		}
+
+		currentBuzzObject = new buzz.sound(song.audioUrl, {
 			formats: ['mp3'],
 			preload: true
 		});
 
-		currentBuzzObject.play();
+		currentSong = song;
+	};
+
+	/**
+	@function playSong
+	@desc plays song that is clicked and sets playing song to true
+	@param {Object} song
+	**/
+
+	var playSong = function(song) {
+		currentBuzzObject.play(song);
+		song.playing = true;
+	};
+
+	/**
+	@function SongPlayer.play
+	@desc if current playing song !== clicked song, stop current song and play clicked
+		OR if current song is paused and was clicked again, play
+	@param {Object} song
+	**/
+
+	SongPlayer.play = function(song) {
+		if (currentSong !== song) {
+			setSong(song);
+			playSong();
+
+		} else if (currentSong === song) {
+			if (currentBuzzObject.isPaused()) {
+				currentBuzzObject.play();
+			}
+		}
+	};
+
+	/**
+	@function SongPlayer.pause
+	@desc pause song and set song playing to false 
+	@param {Object} song
+	**/
+
+	SongPlayer.pause = function(song) {
+		currentBuzzObject.pause();
+		song.playing = false;
 	};
 
 	return SongPlayer;
